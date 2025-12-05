5a1487ce0a21407d135c16ddfaf0142effeab304
Tanvir Anjum <tanviranjum010@gmail.com>
2025-12-05 18:42:05 +0600

    holla gitu

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json({ limit: "5mb" })); // req.body
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(
+  cors({ origin: [ENV.CLIENT_URL, "http://localhost:5173"], credentials: true })
+);
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
@@ -27,10 +29,7 @@ if (ENV.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
-app.get("/test", (req, res) => {
-  // console.log("Test api called");
-  res.json({ message: "Api is working fine!" });
-});
+
 server.listen(PORT, () => {
   console.log("Server running on port: " + PORT);
   connectDB();
