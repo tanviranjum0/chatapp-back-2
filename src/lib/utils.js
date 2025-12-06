42ef7937383c625f934135a5a8404a67135ffe2e
Tanvir Anjum <tanviranjum010@gmail.com>
2025-12-06 20:48:06 +0600

    holla

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -15,7 +15,7 @@ export const generateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks: cross-site scripting
     // sameSite: "", // CSRF attacks
-    secure: ENV.NODE_ENV === "development" ? false : true,
+    // secure: ENV.NODE_ENV === "development" ? false : true,
   });
 
   return token;
