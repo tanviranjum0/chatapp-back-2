f606937156622d4745c0120bee3130918baf7e0f
Tanvir Anjum <tanviranjum010@gmail.com>
2025-12-06 21:04:44 +0600

    holla

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -13,7 +13,7 @@ export const generateToken = (userId, res) => {
 
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true, // prevent XSS attacks: cross-site scripting
+    // httpOnly: true, // prevent XSS attacks: cross-site scripting
     // sameSite: "", // CSRF attacks
     // secure: ENV.NODE_ENV === "development" ? false : true,
   });
